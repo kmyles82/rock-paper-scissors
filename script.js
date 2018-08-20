56809d92ec668b1602a1ff26e2a5ec1f596e6aac
Kerry Myles <kmyles82@gmail.com>
2018-08-20 16:40:47 -0500

    created game() to keep score and report winner

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function computerPlay() {
-    random_num = Math.floor(Math.random() * 3)
+    let random_num = Math.floor(Math.random() * 3)
+    if (random_num === 0) {
+        return "rock"
+    } else if (random_num === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+function playerPlay() {
+    let random_num = Math.floor(Math.random() * 3)
     if (random_num === 0) {
         return "rock"
     } else if (random_num === 1) {
@@ -10,16 +21,64 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+
     if (playerSelection === computerSelection) {
-        return "It's a tie"
-    } else if (computerSelection === 'paper') {
-        return "You Lose! Paper beats Rock"
-    } else {
-        return "You Win! Rock beats Scissors"
+        console.log( "It's a tie")
+        return null
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log( "You Lose! Paper beats Rock")
+        return false
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log( "You Win! Rock beats Scissors")
+        return true
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log( "You Win! Paper beats Rock")
+        return true
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        console.log( "You Lose! Scissors beats Paper")
+        return false
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log( "You Win! Scissors beats Paper")
+        return true
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log( "You Lose! Rock beats Scissors")
+        return false
     }
 }
 
-const playerSelection = prompt("Enter paper, rock or scissors").toLocaleLowerCase()
-const computerSelection = computerPlay()
+function game() {
+    let playerScore = 0
+    let computerScore = 0
 
-console.log(playRound(playerSelection,computerSelection))
+    for (var i = 0; i < 5; i++){
+        let playerSelection = prompt("What is your roll?")
+        let computerSelection = computerPlay()
+
+        console.log(playerSelection)
+        console.log(computerSelection)
+        
+        if (playRound(playerSelection, computerSelection) === null) {
+            playerScore = playerScore
+        }
+        else if (playRound(playerSelection, computerSelection)) {
+            playerScore += 1
+            
+        } else {
+            computerScore += 1
+        } 
+    }
+
+    if (playerScore > computerScore) {
+        return "Player is the winner with the score of " + playerScore + " vs the Computer score of " + computerScore
+    } else {
+        return "Computer is the winner with the score of " + computerScore + " vs the Player score of " + playerScore 
+    }
+}
+
+
+
+
+// console.log(playRound(playerSelection,computerSelection))
+console.log(game())
+console.log("---------------------")
